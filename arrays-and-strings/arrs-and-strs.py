476d6855bd704ef5922137117e29f9e73809d950
EvyHaan <evy.haan@gmail.com>
2019-06-14 12:14:09 -0700

    1.2 permutations complete

--- arrays-and-strings/arrs-and-strs.py
+++ arrays-and-strings/arrs-and-strs.py
@@ -46,11 +46,103 @@ def unique_chars2(string):
     return True
 
 
-if __name__ == "__main__":
-    string1 = 'string'
-    string2 = 'unique'
+"""
+1.2
+Given 2 strings, determine if one is a permutation of the other
 
-    print(unique_chars1(string1))
-    print(unique_chars1(string1))
-    print(unique_chars2(string2))
-    print(unique_chars2(string2))
+Examples
+--------
+input: 'listen' 'silent'
+output: True
+
+input: 'loot' 'tool'
+output: True
+
+input: 'listens' 'silent'
+output: False
+
+input: 'loot', 'look'
+output: False
+
+"""
+
+# SOLUTION 1 - HASHTABLE
+# Efficiency
+# space: O(n)  time: O(2n)
+
+def permutuation_ht(str1, str2):
+
+    if len(str1) != len(str2):
+      return False
+  
+    d = {}
+
+    for char in str1:
+      if char in d:
+        d[char] += 1
+      else:
+        d[char] = 1
+    
+    for char in str2:
+
+      if char in d:
+
+        if d[char] > 0:
+          d[char] -= 1
+        else:
+          return False
+
+      else:
+        return False
+
+    return True
+
+
+# SOLUTION 2 - SORT & COMPARE
+# Efficiency
+# space: O(1)  time: O(6n log n)
+
+def permutuation_ht(str1, str2):
+
+    if len(str1) != len(str2):
+      return False
+  
+    l1 = list(str1)
+    l2 = list(str2)
+
+    l1.sort()
+    l2.sort()
+
+    for i in range(len(str1)):
+      if l1[i] != l2[i]:
+        return False
+    
+    return True
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    # Tests 1.1
+    # string1 = 'string'
+    # string2 = 'unique'
+    # print(unique_chars1(string1))
+    # print(unique_chars1(string1))
+    # print(unique_chars2(string2))
+    # print(unique_chars2(string2))
+
+    #Tests 2.1
+    # string1 = 'listen'
+    # string2 = 'silent'
+    # string1 = 'loot'
+    # string2 = 'tool'
+    # string1 = 'listens'
+    # string2 = 'silent'
+    # string1 = 'loot'
+    # string2 = 'look'
+    # string1 = 'loot'
+    # string2 = 'tools'
+    # print(permutuation_ht(string1, string2))
